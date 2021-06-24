feccffc8459d49f690929fed186342924652b932
原田篤 <haradaatsushi@haradaatsushinoMacBook-Air.local>
2021-06-24 13:58:09 +0900

    header作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,9 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+//
+//= require rails-ujs
+//= require turbolinks
+//= require jquery/dist/jquery.js
+//= require bootstrap/dist/js/bootstrap.min
+//= require_tree .
