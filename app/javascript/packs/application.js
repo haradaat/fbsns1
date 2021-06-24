90cdeab5cfaaaea70147de59661ec55f3e22b17b
原田篤 <haradaatsushi@haradaatsushinoMacBook-Air.local>
2021-06-24 15:02:45 +0900

    application.js変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,9 +11,6 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-//
-//= require rails-ujs
-//= require turbolinks
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
