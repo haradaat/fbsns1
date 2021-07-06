8b5995ead53142466e0a03e85a35a89ccd7b5737
Atsushi Harada <haradaats@gmail.com>
2021-07-06 22:23:10 +0900

    Fixed feed form

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -13,23 +13,27 @@ class FeedsController < ApplicationController
 
   # GET /feeds/new
   def new
+    if params[:back]
+      @feed = Feed.new(feed_params)
+    else
       @feed = Feed.new
+    end
   end
   def confirm
     @feed = Feed.new(feed_params)
+    @feed.user_id = current_user.id
+    render :new if @feed.invalid?
   end
 
   # POST /feeds or /feeds.json
   def create
-    @feed = current_user.feeds.build(feed_params)
-    @user = @feed.user.name
+    @feed = Feed.new(feed_params)
+    
     if params[:back]
       render :new
     else
     if @feed.save
     redirect_to feeds_path, notice: "写真を投稿しました！"
-    else
-      render :new
     end
   end
 end
@@ -45,10 +49,7 @@ end
   # DELETE /feeds/1 or /feeds/1.json
   def destroy
     @feed.destroy
-    respond_to do |format|
-      format.html { redirect_to feeds_url, notice: "Feed was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to feeds_path, notice:"ブログを削除しました！"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
   get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :edit ,:update]
   root 'blogs#index'
   resources :blogs do
     collection do
